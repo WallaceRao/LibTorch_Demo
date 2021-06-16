8cf3d2ca03973d5edf66b52376ff9b348470d299
Yonghui Rao <raoyonghui0630@gmail.com>
2021-06-16 11:19:55 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(100, 120) 
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        y = F.relu(self.fc2(x))
+        z = self.fc3(y)
+        return (x,y,z,100)
+
+
+
+net = Net()
+print(net)
+
+sm = torch.jit.script(net)
+print(sm)
+torch.jit.save(sm, 'test.jit')
+print(x)
